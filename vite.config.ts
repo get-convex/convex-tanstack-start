fe9c4b3f9e9f618fff9cfbac3500503d17445044
Nicolas Ettlin <nicolas@convex.dev>
2025-09-26 15:44:38 -0700

    Use Nitro v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -15,7 +15,7 @@ export default defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
-    nitroV2Plugin(),
+    nitro(),
     viteReact(),
   ],
 })
