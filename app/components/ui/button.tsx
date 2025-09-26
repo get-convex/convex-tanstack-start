99231c7c880ad3419807ff723a843d5453d285c9
Nicolas Ettlin <nicolas@convex.dev>
2025-09-26 14:20:12 -0700

    Update to Tailwind 4

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,15 +5,15 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '~/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-slate-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-slate-700 text-white shadow hover:bg-slate-800',
-        destructive: 'bg-red-600 text-white shadow-sm hover:bg-red-500',
+        default: 'bg-slate-700 text-white shadow-sm hover:bg-slate-800',
+        destructive: 'bg-red-600 text-white shadow-xs hover:bg-red-500',
         outline:
-          'border border-slate-200 bg-white shadow-sm hover:bg-slate-100 hover:text-slate-900',
-        secondary: 'bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-200',
+          'border border-slate-200 bg-white shadow-xs hover:bg-slate-100 hover:text-slate-900',
+        secondary: 'bg-slate-100 text-slate-900 shadow-xs hover:bg-slate-200',
         ghost: 'hover:bg-slate-100 hover:text-slate-900',
         link: 'hover:underline',
       },
